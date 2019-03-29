decc6b055ccf80cba234cce545f1b23977fa28e6
caowei <caowei.max@bytedance.com>
2019-03-29 19:52:28 +0800

    master - add sh
    Change-Id: I82fe09b8a50117ee757164785c3761999b57bb78

---
+++ python/for循环.py
@@ -0,0 +1,9 @@
+for letter in 'Python':
+    print(letter)
+
+for index in [1, 2, 3]:
+    print(index)
+
+for num in range(10, 20):
+    print(num)
+
---
+++ python/字符串.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+name = 'caowei'
+
+# 长度
+print(len(name))
+
+# 拼接
+print(name + ' nihao')
+
+# str
+print(name[1:40])
+
+# split
+print(name.split())
+
+# 居中显示
+print(name.center(20))
