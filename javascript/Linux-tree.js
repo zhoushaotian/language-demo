c69cea6ba7183f0bbd6f61f5533f840a174c95e8
caowei <caowei.max@bytedance.com>
2019-04-03 10:07:19 +0800

    master - update comment
    Change-Id: I92e034e7f4f109a6f6f40a6456c67405831ca10e

--- javascript/Linux-tree.js
+++ javascript/Linux-tree.js
@@ -6,6 +6,7 @@ const space = ' ';
 const indent = 2;
 const gap = 3;
 const gapBeforeName = 4;
+const breakLine = '\n';
 
 
 function treeView(array, isLast = true, prefix = '') {
@@ -15,23 +16,35 @@ function treeView(array, isLast = true, prefix = '') {
         if (!isString) {
             let appendix = `${prefix}${(isLast ? L : T) + horizontal.repeat(indent)}`;
             let passedPrefix = prefix + (isLast ? (space.repeat(gapBeforeName)) : (vertical + space.repeat(gap)));
-            return `${appendix} ${item.name}\n${treeView(item.children, isLast, passedPrefix)}`
+            return `${appendix} ${item.name}${breakLine}${treeView(item.children, isLast, passedPrefix)}`;
         } else {
             if (isLast) {
-                return `${prefix}${L}${horizontal.repeat(indent)} ${item}`;
+                return `${prefix}${L}${horizontal.repeat(indent)}${space}${item}`;
             } else {
-                return `${prefix}├── ${item}`
+                return `${prefix}${T}${horizontal.repeat(2)}${space}${item}`
             }
         }
-    }).join('\n')
+    }).join(breakLine);
 }
 
-
+console.log('****');
 console.log(treeView(['a', 'b', 'c']));
 
+// ├── a
+// ├── b
+// └── c
+
+console.log('****');
 console.log(treeView(['a', 'b', 'c',{name:'d',children:['ss','ddd']}]));
 
+// ├── a
+// ├── b
+// ├── c
+// └── d
+//     ├── ss
+//     └── ddd
 
+console.log('****');
 console.log(treeView([
     'folder',
     'ddd',
@@ -43,4 +56,72 @@ console.log(treeView([
             'b',
             'c',
             {name: 'caowei', children: ['xxx', 'uuuu']}]
-    }, 'fuck', {name: 'xxxsdf', children: ['dddd']}, 'yes', {name: 'caowei', children: ['xxx', 'uuuu']}]));
+    }, 'pk', {name: 'xxxsdf', children: ['dddd']}, 'yes', {name: 'caowei', children: ['xxx', 'uuuu']}]));
+
+// ├── folder
+// ├── ddd
+// ├── dddddcd
+// ├── wewewe
+// │   ├── a
+// │   ├── caowei
+// │   │   ├── xxx
+// │   │   └── uuuu
+// │   ├── b
+// │   ├── c
+// │   └── caowei
+// │       ├── xxx
+// │       └── uuuu
+// ├── pk
+// ├── xxxsdf
+// │   └── dddd
+// ├── yes
+// └── caowei
+//     ├── xxx
+//     └── uuuu
+
+console.log('****');
+
+console.log(treeView([{name:'Container',children:
+        [
+            {name:'Stage',children:[
+                'Stage-Header',
+                    {
+                        name:'Stage-Main',
+                        children:[
+                            {name:'GridView',children:[
+                                    {name:'DraggableGrid',children:[{name:'GridNode',children:[
+                                                'ScaffoldStart',
+                                                {name:'GridNodeContent',children:[
+                                                    'IndentStart',
+                                                        {name:'Content',children:['ContentRenderer']}
+                                                    ]},
+                                                'ScaffoldEnd'
+                                            ]}]}
+                                ]}
+                        ]
+                    }
+                ]},
+            {name:'Sidebar',children:['EmployeeList','ProjectOutline']},
+            'ProjectDetail',
+            'CustomerService'
+            ]
+}]));
+
+// └── Container
+//     ├── Stage
+//     │   ├── Stage-Header
+//     │   └── Stage-Main
+//     │       └── GridView
+//     │           └── DraggableGrid
+//     │               └── GridNode
+//     │                   ├── ScaffoldStart
+//     │                   ├── GridNodeContent
+//     │                   │   ├── IndentStart
+//     │                   │   └── Content
+//     │                   │       └── ContentRenderer
+//     │                   └── ScaffoldEnd
+//     ├── Sidebar
+//     │   ├── EmployeeList
+//     │   └── ProjectOutline
+//     ├── ProjectDetail
+//     └── CustomerService
