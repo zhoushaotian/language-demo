7278e57aaec34264319386b3318a368234ea07bc
caowei <caowei.max@bytedance.com>
2019-05-06 17:02:53 +0800

    master - add latest call
    Change-Id: I7fb7bbab1b801541238825bb93acb121eeb91dae

--- javascript/latestCall.js
+++ javascript/latestCall.js
@@ -9,15 +9,12 @@ function latestCall(fn) {
   return (...args) => {
     counter++;
     return new Promise(resolve => {
-      fn.apply(this, args).then((value={}) => {
-
-        if(currentLatestId >= counter){
+      fn.apply(this, args).then((value = {}) => {
+        if (currentLatestId >= counter) {
           return;
         }
         value['counter'] = counter;
-        resolve({
-
-        });
+        resolve({});
       });
     });
   }
@@ -27,13 +24,14 @@ function latestCall(fn) {
 function requestMock(i) {
   return new Promise(resolve => {
     setTimeout(() => {
+      console.log('x:' + i);
       resolve(i);
     }, Math.floor(Math.random() * 3000));
   });
 }
 
 
-let latestRequest = requestMock; //latestCall(requestMock);
+let latestRequest = latestCall(requestMock); //latestCall(requestMock);
 
 function test() {
   for (let i = 0; i < 10; i++) {
